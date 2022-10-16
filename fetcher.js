1ab1b2240b5160e87f86a9376439b207b079624e
Lauren Ashley <la.zaubi@gmail.com>
2022-10-15 20:06:53 -0700

    started setup for page fetcher

--- fetcher.js
+++ fetcher.js
@@ -1,1 +1,18 @@
+// fetcher.js
 
+const args = process.argv.slice(2);
+const fs = require('fs/promises');
+
+console.log(args);
+
+// make http request
+
+// take data received and write to local file
+const content = 'Some content!';
+
+fs.writeFile('/Users/joe/test.txt', content, err => {
+  if (err) {
+    console.error(err);
+  }
+  // file written successfully
+});

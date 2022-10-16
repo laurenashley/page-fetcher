13239490f213d8f713cf03954db4288331e89344
Lauren Ashley <la.zaubi@gmail.com>
2022-10-15 21:20:53 -0700

    started adding first stretch case for if local file alredy exits. getting an error currently, committing progress before going to bed

--- fetcher.js
+++ fetcher.js
@@ -1,18 +1,49 @@
 // fetcher.js
 
-const args = process.argv.slice(2);
 const fs = require('fs/promises');
 
-console.log(args);
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const args = process.argv.slice(2);
+const content = args[0];
+const localFilePth = args[1];
+
+let bytes = 0;
+
+const userAnswer = () => {
+  let answer;
+
+  return answer;
+};
+
+const writeNewFile = (body) => {
+  fs.writeFile(localFilePth, body, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+  console.log(`Downloaded and saved ${bytes} bytes to ${localFilePth}`);
+};
 
 // make http request
+const request = require('request');
+request(content, (error, response, body) => {
+  bytes = body.length;
 
-// take data received and write to local file
-const content = 'Some content!';
-
-fs.writeFile('/Users/joe/test.txt', content, err => {
-  if (err) {
+  try {
+    if (fs.existsSync(localFilePth)) {
+      console.log('file exists');
+      if (userAnswer) {
+        writeNewFile(body);
+      }
+    } else {
+      writeNewFile(body);
+    }
+  } catch (err) {
     console.error(err);
   }
-  // file written successfully
 });
